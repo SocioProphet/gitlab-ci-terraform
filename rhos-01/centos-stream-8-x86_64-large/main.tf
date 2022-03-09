f85d781b309790f910838ffe52e61b77adf263da
Jakub Rusz <jrusz@redhat.com>
2022-03-09 13:14:17 +0100

    rhos-01/centos-8: update images to 20220125.1

--- rhos-01/centos-stream-8-x86_64-large/main.tf
+++ rhos-01/centos-stream-8-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-8"
-  image_id  = "0285b667-3d71-4e8d-a66d-a0442bae9116"
+  image_id  = "ac235d90-4721-4061-9622-9d43204392f6"
   flavor_id = "c3ec7a0a-0443-4253-a6ab-040cc0278ced"
 }
 
--- rhos-01/centos-stream-8-x86_64/main.tf
+++ rhos-01/centos-stream-8-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "centos-stream-8"
-  image_id  = "0285b667-3d71-4e8d-a66d-a0442bae9116"
+  image_id  = "ac235d90-4721-4061-9622-9d43204392f6"
   flavor_id = "f2c4469b-f516-46d1-8b87-1dcca68fb3d9"
 }
 
